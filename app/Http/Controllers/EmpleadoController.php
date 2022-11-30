294dc12803ca49b9ed45d7a413e832a4ac051e04
johanna <johasistems@gmail.com>
2022-11-29 21:33:40 -0500

    inicio

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+
+/**
+ * Class EmpleadoController
+ * @package App\Http\Controllers
+ */
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $empleados = Empleado::paginate();
+
+        return view('empleado.index', compact('empleado'))
+            ->with('i', (request()->input('page', 1) - 1) * $empleados->perPage());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $empleado = new Empleado();
+        return view('empleado.create', compact('empleado'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate(Empleado::$rules);
+
+        $empleado = Empleado::create($request->all());
+
+        return redirect()->route('empleados.index')
+            ->with('success', 'Empleado created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $empleado = Empleado::find($id);
+
+        return view('empleado.show', compact('empleado'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $empleado = Empleado::find($id);
+
+        return view('empleado.edit', compact('empleado'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  Empleado $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Empleado $empleado)
+    {
+        request()->validate(Empleado::$rules);
+
+        $empleado->update($request->all());
+
+        return redirect()->route('empleados.index')
+            ->with('success', 'Empleado updated successfully');
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy($id)
+    {
+        $empleado = Empleado::find($id)->delete();
+
+        return redirect()->route('empleado.index')
+            ->with('success', 'Empleado deleted successfully');
+    }
+}
